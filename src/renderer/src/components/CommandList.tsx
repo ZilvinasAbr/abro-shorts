7186524b2e4649d181fb1fb6304b4cf3161194dc
Zilvinas <abromavicius.zilvinas@gmail.com>
2026-02-02 08:25:38 +0200

    fix: add min-h-0 to ScrollArea to enable command list scrolling

--- src/renderer/src/components/CommandList.tsx
+++ src/renderer/src/components/CommandList.tsx
@@ -68,7 +68,7 @@ export function CommandList({ commands }: CommandListProps): React.JSX.Element {
   }
 
   return (
-    <ScrollArea className="flex-1">
+    <ScrollArea className="flex-1 min-h-0">
       <div ref={listRef} className="space-y-1 p-2">
         {commands.map((command, index) => (
           <div key={command.id} data-index={index}>
